71eba7b8fd8dbe15f1c7f819d42a47c7965e24a3
ndelvalle <nicolas.delvalle@gmail.com>
2016-10-26 04:56:08 +0700

    X2-1326 Refactor using fetch

--- src/index.js
+++ src/index.js
@@ -1,100 +1,91 @@
-import axios            from 'axios';
+import 'whatwg-fetch';
 import { EventEmitter } from 'events';
-
 
 class HTTP extends EventEmitter {
   constructor() {
     super();
-    this._middlewares  = [];
-    this._timeout      = 10000;
-    this.prefixEmitter = '';
+
+    this._baseUrl     = '';
+    this._middlewares = [];
+  }
+
+  static _fetchOptions(opts = {}) {
+    return {
+      method : opts.method || 'GET',
+      body   : opts.data ? JSON.stringify(opts.data) : undefined,
+      headers: { 'Content-Type': 'application/json' }
+    };
   }
 
   init(opts = {}) {
-    if (!opts.baseURL) { throw new Error('Unable to init http service, missing baseURL attribute'); }
-    if (opts.timeout) { this._timeout = opts.timeout; }
-    if (opts.prefixEmitter) {
-      this._prefixEmitter = opts.prefixEmitter;
-      delete opts.prefixEmitter;
-    }
-    if (opts.middlewares) {
-      this._middlewares = opts.middlewares;
-      delete opts.middlewares;
-    }
-    this._axios = axios.create(opts);
+    this._baseUrl     = opts.baseURL;
+    this._middlewares = opts.middlewares || [];
   }
 
-  async get(path, params, config = {}, auth = true) {
-    if (!this._axios) { throw new Error('Library has not being initialized'); }
-
-    config.params = params;
+  async get(path, params, auth = true) {
     try {
-      return await this._axios.get(path, this._runMiddlewares(config, auth));
+      const url    = `${this._baseUrl}${path}`;
+      const config = this._runMiddlewares(this.constructor._fetchOptions({ body: params }));
+
+      const res  = await fetch(url, config);
+      const body = await res.json();
+
+      return this.constructor.responseHandler(res, body);
     } catch (err) {
-      const message = this.constructor.errorHandler(err);
-      this.emit(`${this._prefixEmitter}-http-error`, message);
-      throw (message);
+      this.emit('http-client:error', err);
+      throw err;
     }
   }
 
-  async post(path, data, config = {}, auth = true) {
-    if (!this._axios) { throw new Error('Library has not being initialized'); }
+  async post(path, data, auth = true) {
     try {
-      return await this._axios.post(path, data, this._runMiddlewares(config, auth));
+      const url    = `${this._baseUrl}${path}`;
+      const config = this._runMiddlewares(this.constructor._fetchOptions({ body: data, method: 'POST' }));
+
+      const res  = await fetch(url, config);
+      const body = await res.json();
+
+      return this.constructor.responseHandler(res, body);
     } catch (err) {
-      const message = this.constructor.errorHandler(err);
-      this.emit(`${this._prefixEmitter}-http-error`, message);
-      throw (message);
+      this.emit('http-client:error', err);
+      throw err;
     }
   }
 
-  async put(path, data, config = {}, auth = true) {
-    if (!this._axios) { throw new Error('Library has not being initialized'); }
+  async put(path, data, auth = true) {
     try {
-      return await this._axios.put(path, data, this._runMiddlewares(config, auth));
+      const url    = `${this._baseUrl}${path}`;
+      const config = this._runMiddlewares(this.constructor._fetchOptions({ body: data, method: 'PUT' }));
+
+      const res  = await fetch(url, config);
+      const body = await res.json();
+
+      return this.constructor.responseHandler(res, body);
     } catch (err) {
-      const message = this.constructor.errorHandler(err);
-      this.emit(`${this._prefixEmitter}-http-error`, message);
-      throw (message);
+      this.emit('http-client:error', err);
+      throw err;
     }
   }
 
   async del(path, config = {}, auth = true) {
-    if (!this._axios) { throw new Error('Library has not being initialized'); }
     try {
-      return await this._axios.delete(path, this._runMiddlewares(config, auth));
+      const url    = `${this._baseUrl}${path}`;
+      const config = this._runMiddlewares(this.constructor._fetchOptions({ method: 'DELETE' }));
+
+      const res  = await fetch(url, config);
+      const body = await res.json();
+
+      return this.constructor.responseHandler(res, body);
     } catch (err) {
-      const message = this.constructor.errorHandler(err);
-      this.emit(`${this._prefixEmitter}-http-error`, message);
-      throw (message);
+      this.emit('http-client:error', err);
+      throw err;
     }
   }
 
-  static errorHandler(response) {
-    if (response instanceof Error) { return `Unknown Error: ${response}`; }
-    if (!response.status) { return 'Unknown Error'; }
-
-    switch (response.status) {
-      case 400:
-        return response.data || response;
-      case 401:
-        if (response.data && response.data.length > 0) { return response.data[0].message; }
-        break;
-      case 402:
-        return 'Error 402: You must upgrade your account to do that';
-      case 403:
-        return 'Error 403: You are not authorized to access that';
-      case 404:
-        return 'Requested Resource Not Found';
-      case 429:
-        return 'Rate Limited';
-      case 500:
-      case 502:
-      case 503:
-        return `API Server Error ${response.status}`;
-      default:
-        return `API Request Error ${response.status}`;
-    }
+  static responseHandler(res, body) {
+    if (res.status > 500) { throw new Error(`API Server Error ${res.status}`); }
+    if (res.status > 300) { throw new Error(body || res.statusText); }
+    return body;
   }
 
   _runMiddlewares(config, auth) {
